60b26efb37f4849d72d5b7df584324f38daf5676
io Gi <62535762+yogyy@users.noreply.github.com>
2022-11-12 16:56:46 +0700

    Add files via upload

---
+++ codewars/bitcounting.js
@@ -0,0 +1,9 @@
+var countBits = function (n) {
+  // make an array with the bit result
+  const base = n.toString(2).split("");
+
+  // make a sum with the array and make the index a Number
+  const result = base.reduce((sum, num) => sum + Number(num), 0);
+
+  return result;
+};
