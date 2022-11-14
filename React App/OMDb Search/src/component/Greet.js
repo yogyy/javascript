a523b2776a037215d771c0cc8ac0c2525deabafc
io Gi <62535762+yogyy@users.noreply.github.com>
2022-11-14 20:37:14 +0700

    src
    create-react-app
    
    replace src folder

---
+++ React App/OMDb Search/src/component/Greet.js
@@ -0,0 +1,11 @@
+function Greet() {
+  const power = "Force";
+
+  return (
+    <>
+      <p>May The {power} be With You</p>
+    </>
+  );
+}
+
+export default Greet;
---
+++ React App/OMDb Search/src/component/Welcome.js
@@ -0,0 +1,9 @@
+function Welkom(props) {
+  return (
+    <>
+      <h1>Welcome to {props.name}</h1>
+    </>
+  );
+}
+
+export default Welkom;
