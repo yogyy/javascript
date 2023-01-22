eafdba020b7ce6d8aa66a89f1fede43ea0809fc0
yogy Cons'tantine <62535762+yogyy@users.noreply.github.com>
2023-01-23 02:58:27 +0700

    Create diagonalDifference.js

---
+++ hackerrank./diagonalDifference.js
@@ -0,0 +1,9 @@
+ function diagonalDifference(arr) {
+    let primaryDiagonal = 0;
+    let secondaryDiagonal = 0;
+    for (let i = 0; i < arr.length; i++) {
+        primaryDiagonal += arr[i][i];
+        secondaryDiagonal += arr[i][arr.length - i - 1];
+    }
+    return Math.abs(primaryDiagonal - secondaryDiagonal);
+}
