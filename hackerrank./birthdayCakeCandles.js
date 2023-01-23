03905c511d2be151a92e97d0e2821daa8a3ddfc7
yogy Cons'tantine <62535762+yogyy@users.noreply.github.com>
2023-01-23 23:17:56 +0700

    Create birthdayCakeCandles.js

---
+++ hackerrank./birthdayCakeCandles.js
@@ -0,0 +1,18 @@
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let blownCandleCount = 0;
+    
+    let max = 0;
+    
+    for(let i=0; i<candles.length; i++){
+        if(candles[i]> max){
+            max = candles[i];
+            blownCandleCount = 1;
+        } else{
+            if(max==candles[i]){
+                blownCandleCount++;
+            }
+        }
+    }
+    return blownCandleCount;
+}
