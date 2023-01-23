3dced99f70b8997f04f9306ec1c785e3a8a5aeea
yogy Cons'tantine <62535762+yogyy@users.noreply.github.com>
2023-01-23 20:27:45 +0700

    Create sum String as Number.js

---
+++ codewars/sum String as Number.js
@@ -0,0 +1,5 @@
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+function sumStrings(a,b) { 
+  return (BigInt(a) + BigInt(b)).toString()
+}
