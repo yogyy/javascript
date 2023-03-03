fb8dd69662e7fc06bc2f00cc7562c56a37da9e51
yogy Cons'tantine <62535762+yogyy@users.noreply.github.com>
2023-03-03 15:38:35 +0700

    Palindrome Number

---
+++ leetcode/0009-palindrome-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  // Creates array from int characters
+  // 121 -> [1,2,1]
+  let arr = Array.from(String(x), Number);
+
+  // Uses two pointer
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[arr.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
