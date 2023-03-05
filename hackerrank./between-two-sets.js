dfb36fe3adb2cccaf3edd82a5bf86395d565564c
yogy Cons'tantine <62535762+yogyy@users.noreply.github.com>
2023-03-05 14:33:03 +0700

    Between Two Sets

---
+++ hackerrank./between-two-sets.js
@@ -0,0 +1,28 @@
+function getTotalX(a, b) {
+  function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+  }
+
+  function gcd(a, b) {
+    // Euclid's division algorithm
+    let t;
+    while (b !== 0) {
+      t = b;
+      b = a % b;
+      a = t;
+    }
+    return a;
+  }
+
+  let l = a.reduce(lcm),
+    g = b.reduce(gcd),
+    c = 0;
+
+  for (let i = l; i < g + 1; i += l) {
+    if (g % i === 0) {
+      ++c;
+    }
+  }
+
+  return c;
+}
