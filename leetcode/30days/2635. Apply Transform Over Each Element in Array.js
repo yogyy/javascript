7a96fd10aa9a28dd9cef996d082f89785fc000bd
Yogi F S <yogimr261@gmail.com>
2023-09-03 20:08:20 +0700

    Create 2635. Apply Transform Over Each Element in Array.js

---
+++ leetcode/30days/2635. Apply Transform Over Each Element in Array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const res = new Array(arr.length)
+    for(const i in arr){
+        res[i] = fn(arr[i], Number(i))
+    }
+    return res
+};
+
+
+
+var map = function(arr, fn) {
+    const result = [];
+    
+};
