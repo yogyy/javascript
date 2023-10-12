39d89fba6413416d12268674fbf649a2c38e5f2d
Yogi F S <yogimr261@gmail.com>
2023-10-12 22:17:33 +0700

    add Remove Duplicates from Sorted Array

---
+++ leetcode/0026-Remove Duplicates from Sorted Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let index = 1
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            nums[index] = nums[i + 1]
+            index++
+        }
+    }
+
+    return index
+};
