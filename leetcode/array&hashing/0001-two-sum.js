0b00372294bc492d136bd48a85b80259d6b71d04
Yogi <yogimr261@gmail.com>
2024-02-11 01:15:09 +0700

    fix: move to neetcode structure

---
+++ leetcode/array&hashing/0001-two-sum.js
@@ -0,0 +1,39 @@
+const twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
+
+/**
+ *
+ * @param {Array<number>} arr
+ * @param {number} target
+ */
+var twoSum = function (arr, target) {
+  const map = {};
+  for (let i = 0; i < arr.length; i++) {
+    map[arr[i]] = i;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let c = target - arr[i];
+    if (map[c] && map[c] !== i) return [i, map[c]];
+  }
+};
+
+/**
+ * brute force
+ * @param {Array<number>} arr
+ * @param {number} target
+ */
+var twoSum = function (arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      return [i, i];
+    }
+  }
+};
